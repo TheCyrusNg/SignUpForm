97d97a213c7bdf3be51dd197243dd0c73dcf99cd
Cyrus Ng <tun60613@temple.edu>
2021-09-21 22:45:24 -0400

    Commit 6

--- app/src/main/java/edu/temple/signupform/FormActivity.kt
+++ app/src/main/java/edu/temple/signupform/FormActivity.kt
@@ -18,7 +18,23 @@ class FormActivity : AppCompatActivity() {
         val confPassword = findViewById<EditText>(R.id.txtConfPassword)
         val duration = Toast.LENGTH_SHORT
 
-        val toast = Toast.makeText(applicationContext, "Name: "+ name.getText().toString() + "Email: " + email.getText().toString() + "Password: " + password.getText().toString(), duration)
+        val toast = Toast.makeText(applicationContext, "Name: "+ name.getText().toString() + "\n Email: " + email.getText().toString() + "\n Password: " + password.getText().toString(), duration)
+
+        if(name.getText().toString() == ""){
+            name.error = "Name no good"
+        }
+        if(email.getText().toString() == ""){
+            email.error = "Email no good"
+        }
+        if(password.getText().toString() == ""){
+            password.error = "Password no good"
+        }
+        if(confPassword.getText().toString() == ""){
+            confPassword.error = "Confirm password no good"
+        }
+        if(!password.getText().toString().equals(confPassword.getText().toString())){
+            confPassword.error = "Confirm password defenitly no good"
+        }
         toast.show()
     }
 }
