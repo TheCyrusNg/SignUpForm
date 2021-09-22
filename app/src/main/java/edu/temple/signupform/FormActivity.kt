51ea30cb8480ff69761d1ddaf2ef452cf36082a1
Cyrus Ng <tun60613@temple.edu>
2021-09-21 22:18:23 -0400

    Commit 5

--- app/src/main/java/edu/temple/signupform/FormActivity.kt
+++ app/src/main/java/edu/temple/signupform/FormActivity.kt
@@ -1,11 +1,24 @@
 package edu.temple.signupform
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class FormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun onBtnClick(view: View?){
+        val name = findViewById<EditText>(R.id.txtName)
+        val email = findViewById<EditText>(R.id.txtEmail)
+        val password = findViewById<EditText>(R.id.txtPassword)
+        val confPassword = findViewById<EditText>(R.id.txtConfPassword)
+        val duration = Toast.LENGTH_SHORT
+
+        val toast = Toast.makeText(applicationContext, "Name: "+ name.getText().toString() + "Email: " + email.getText().toString() + "Password: " + password.getText().toString(), duration)
+        toast.show()
     }
 }
